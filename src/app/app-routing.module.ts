89621e87b6e4ba9101c2fec548774b686e30c04d
Joseph Georges <jgeorges@mawhiba.org.sa>
2023-01-03 22:46:44 +0200

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'Sales', loadChildren: () => import('./sales/sales.module').then(m => m.SalesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
